cf4c56ef0125b4f1c7165ff1cbfcf818e36c4958
KOLO6Au <60756774+KOLO6Au@users.noreply.github.com>
2021-08-29 20:40:44 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1630255505,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/sprite-sheet3.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/asound1.m4a",
+		"media/asound1.ogg",
+		"media/asound2.m4a",
+		"media/asound2.ogg",
+		"media/asound3.m4a",
+		"media/asound3.ogg",
+		"media/asound4.m4a",
+		"media/asound4.ogg",
+		"media/asound5.m4a",
+		"media/asound5.ogg",
+		"media/asound6.m4a",
+		"media/asound6.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
